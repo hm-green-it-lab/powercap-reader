eb718d75288ffdd96a73a4e3583d4c5048c61750
brunnert <brunnert@hm.edu>
2026-02-25 08:58:25 +0100

    Support higher frequencies

--- src/main/java/edu/hm/greenit/tools/powercap/RaplReader.java
+++ src/main/java/edu/hm/greenit/tools/powercap/RaplReader.java
@@ -3,9 +3,10 @@ package edu.hm.greenit.tools.powercap;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import io.quarkus.scheduler.Scheduled;
-import io.smallrye.common.annotation.RunOnVirtualThread;
+import io.quarkus.virtual.threads.VirtualThreads;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
 
 @ApplicationScoped
 @QuarkusMain
@@ -22,8 +24,25 @@ public class RaplReader implements QuarkusApplication {
 
     private List<String> raplDomains;
 
+    @Inject
+    private ScheduledExecutorService scheduledExecutorService;
+
+    @ConfigProperty(name = "powercap.interval.ms", defaultValue = "1000")
+    private long powerCapIntervalMs;
+
     public static void main(String[] args) {
         Quarkus.run(RaplReader.class, args);
+    }
+
+    // Reads a single line from a file if it exists
+    private static String readFile(String path) throws IOException {
+        if (Files.exists(Path.of(path))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+                return reader.readLine();
+            }
+        } else {
+            return "-1";
+        }
     }
 
     @Override
@@ -33,6 +52,14 @@ public class RaplReader implements QuarkusApplication {
 
         // Print CSV header for raw mode
         System.out.println("Timestamp,Domain, Energy (micro joules), DRAM Energy (micro joules)");
+
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            try {
+                readPowercapDataAndWriteTofiles();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }, 0, powerCapIntervalMs, java.util.concurrent.TimeUnit.MILLISECONDS);
 
         // Keep application running
         while (true) {
@@ -49,20 +76,6 @@ public class RaplReader implements QuarkusApplication {
                 .toList();
     }
 
-    // Reads a single line from a file if it exists
-    private static String readFile(String path) throws IOException {
-        if (Files.exists(Path.of(path))) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-                return reader.readLine();
-            }
-        } else {
-            return "-1";
-        }
-    }
-
-    // Scheduled reader for raw mode
-    @RunOnVirtualThread
-    @Scheduled(cron = "${powercap.cron}", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
     void readPowercapDataAndWriteTofiles() throws IOException {
         long timestamp = System.currentTimeMillis();
 
